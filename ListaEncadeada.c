c8d8ffa389d0be7026e5d56cf84f0a6598891340
NatyBernini <110041227+NatyBernini@users.noreply.github.com>
2024-03-19 19:56:02 -0300

    Update ListaEncadeada.c

--- ListaEncadeada.c
+++ ListaEncadeada.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
     int id;
@@ -17,7 +19,7 @@ typedef struct{
 }TipoLista;
 
 void criar(TipoLista *lista){
-    lista->primeiro = (apontador) malloc(sizeof(celula)); // Aloca espaço na memória para a célula cabeça
+    lista->primeiro = (apontador) malloc(sizeof(celula));
     lista->primeiro->prox = NULL;
     lista->ultimo = lista->primeiro;
 }
@@ -29,28 +31,11 @@ int vazia(TipoLista lista){
 }
 
 void inserirFinal(TipoLista *lista, TipoItem item){
-    lista->ultimo->prox = (apontador) malloc(sizeof(celula));
-    lista->ultimo = lista->ultimo->prox;
-    lista->ultimo->prox = NULL;
-    lista->ultimo->item = item;
-}
-
-void excluir(TipoLista *lista, int idItem){
-    if (vazia(*lista)==1){
-        printf("A lista está vazia");
-    }else{
-        apontador anterior, atual;
-        anterior = lista->primeiro;
-        atual = lista->primeiro->prox; // Ou atual = anterior -> prox
-        while(atual != NULL){
-            if(atual->item.id == idItem){
-                anterior->prox = atual->prox;
-                free(atual);
-                break;
-            }
-            anterior = atual;
-            atual = atual->prox;
-        }
+    if(buscar(*lista, item.id)==0){
+        lista->ultimo->prox = (apontador) malloc(sizeof(celula));
+        lista->ultimo = lista->ultimo->prox;
+        lista->ultimo->prox = NULL;
+        lista->ultimo->item = item;
     }
 }
 
@@ -65,6 +50,103 @@ void imprimir(TipoLista lista){
         }
     }
 }
+
+void remover(TipoLista *lista, int id){
+    if(vazia(*lista)==1){
+        printf("A lista esta vazia!");
+    }else{
+        apontador anterior, atual;
+        anterior = lista->primeiro;
+        atual = lista->primeiro->prox;
+        while(atual!=NULL){
+            if(atual->item.id == id){
+                anterior->prox = atual->prox;
+                free(atual);
+                break;
+            }
+            anterior = atual;
+            atual = atual->prox;
+        }
+    }
+}
+
+int buscar(TipoLista lista, int id){
+    apontador aux;
+    aux = lista.primeiro->prox;
+    while(aux!=NULL){
+        if(aux->item.id == id)
+            return 1;
+        aux = aux->prox;
+    }
+    return 0;
+}
+
+void TrocaItem(TipoLista *lista, int id_1, int id_2){
+    if(vazia(*lista)==1){
+        printf("A lista está vazia");
+    } else {
+        apontador aux = lista -> primeiro -> prox;
+        apontador item_1 = NULL, item_2 = NULL;
+        while (aux!=NULL){
+            if(aux->item.id== id_1){
+                item_1 = aux;
+            }
+            if(aux->item.id== id_2){
+                item_2 = aux;
+            }
+            aux = aux->prox;
+        }
+        if(item_1!=NULL && item_2!=NULL){
+            if (item_1 == item_2){
+                printf("\nVocê inseriu dois IDs iguais!");
+            } else{
+                int temp = item_1->item.id;
+                item_1->item.id = item_2->item.id;
+                item_2->item.id = temp;
+                printf("\nTroca realizada com sucesso!");
+
+                printf("\n\nSegunda impressao: \n");
+                imprimir(*lista);
+            }
+        }else {
+            printf("\nIDs fornecidos nao encontrados na lista!\n");
+        }
+    }
+}
+
+// Função para inserir um novo item na posição indicada
+void inserirPosicao(TipoLista *lista, int id, int id_novo) {
+    apontador atual = lista->primeiro->prox;
+    apontador antecessor = lista->primeiro;
+
+    // Encontra o nó com o id dado
+    while (atual != NULL && atual->item.id != id) {
+        antecessor = atual;
+        atual = atual->prox;
+    }
+
+    // Se o nó com o id dado foi encontrado
+    if (atual != NULL) {
+        // Cria um novo nó com o id_novo
+        apontador novo = (apontador)malloc(sizeof(apontador));
+        if (novo == NULL) { // Quando a memoria está cheia
+            fprintf(stderr, "Erro: memória insuficiente.\n");
+            exit(EXIT_FAILURE);
+        }
+        novo->item.id = id_novo; // ID passado pelo usuário
+        novo->prox = atual; // Posição passada pelo usuário
+
+        // Insere o novo nó antes do nó com o id dado
+        antecessor->prox = novo;
+
+        printf("\n");
+        imprimir(*lista);
+    } else {
+        printf("Erro: o id %d não foi encontrado na lista.\n", id);
+    }
+}
+
+
 
 int main()
 {
@@ -82,18 +164,16 @@ int main()
     item.id = 3;
     inserirFinal(&lista, item);
 
-
     item.id = 4;
     inserirFinal(&lista, item);
 
-
-    item.id = 5;
-    inserirFinal(&lista, item);
-
+    printf("Primeira impressao: \n");
     imprimir(lista);
 
-    excluir(&lista, 3);
+    //TrocaItem(&lista, 2,1);
+    inserirPosicao(&lista, 2, 5);
 
-    imprimir(lista);
+
     return 0;
 }
+
